7512a3c281695a64f39863af340b0921fd33db63
Aaron Dalager <113053870+Ungabad@users.noreply.github.com>
2024-10-16 15:22:23 -0700

    Added some more content and fixed the navbar

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -5,31 +5,22 @@ import { gridItems } from "@/data";
 const Grid = () => {
   return (
     <section id='about'>
-      <BentoGrid>
-        {gridItems.map(
-          ({
-            id,
-            title,
-            description,
-            className,
-            img,
-            imgClassName,
-            titleClassName,
-            spareImg,
-          }) => (
-            <BentoGridItem
-              id={id}
-              key={id}
-              title={title}
-              description={description}
-              className={className}
-              img={img}
-              imgClassName={imgClassName}
-              titleClassName={titleClassName}
-              spareImg={spareImg}
-            />
-          )
-        )}
+      <BentoGrid className='w-full py-20'>
+        {gridItems.map((item, i) => (
+          <BentoGridItem
+            id={item.id}
+            key={i}
+            title={item.title}
+            description={item.description}
+            // remove icon prop
+            // remove original classname condition
+            className={item.className}
+            img={item.img}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            spareImg={item.spareImg}
+          />
+        ))}
       </BentoGrid>
     </section>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -30,7 +30,7 @@ function Hero() {
           </h2>
           <TextGenerateEffect
             className='text-center text-[40px] md:text-5xl lg:text-6xl'
-            words='Transforming Concepts into Seamless Experiences'
+            words='Transforming Concepts into Seamless User Experiences'
           />
           <p className='text-center md:tracking0wider mb-4 text-sm md:text-lg lg:text-2xl'>
             Hi, I&apos;m Aaron, a Next.js Developer based in Spokane, WA USA
