0df9ade239ab06a04ab928026febfded7f23bf96
Aaron Dalager <113053870+Ungabad@users.noreply.github.com>
2024-10-18 16:56:47 -0700

    Added Sentry and Finished Recent Projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 "use client";
-import Grid from "@/components/Grid";
+
+import { navItems } from "@/data";
+
 import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/floating-navbar";
-import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className='relative dark:text-white dark:bg-black-100 bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
       <div className='max-w-7xl w-full'>
-        <FloatingNav
-          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
