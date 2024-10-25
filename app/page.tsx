b9c02b8dcf4456c568abeb359e5933db0a5ccc3b
Aaron Dalager <113053870+Ungabad@users.noreply.github.com>
2024-10-25 16:56:05 -0700

    Finished the build, now need to change data

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,23 @@ import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/floating-navbar";
+import Clients from "@/components/Clients";
+import Experiance from "@/components/Experiance";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className='relative dark:text-white dark:bg-black-100 bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
+    <main className='relative dark:text-white dark:bg-black-100 bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip'>
       <div className='max-w-7xl w-full'>
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients />
+        <Experiance />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
