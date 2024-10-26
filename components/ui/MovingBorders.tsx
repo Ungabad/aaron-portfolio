b47a1bfe8c9c35c2af0b6a177a8251518b02e2f4
Aaron Dalager <113053870+Ungabad@users.noreply.github.com>
2024-10-25 17:38:29 -0700

    Fixing bug

--- components/ui/MovingBorders.tsx
+++ components/ui/MovingBorders.tsx
@@ -1,5 +1,4 @@
-"use client";
-import React from "react";
+import { useRef } from "react";
 import {
   motion,
   useAnimationFrame,
@@ -7,67 +6,6 @@ import {
   useMotionValue,
   useTransform,
 } from "framer-motion";
-import { useRef } from "react";
-import { cn } from "@/lib/utils";
-
-export function Button({
-  borderRadius = "1.75rem",
-  children,
-  as: Component = "button",
-  containerClassName,
-  borderClassName,
-  duration,
-  className,
-  ...otherProps
-}: {
-  borderRadius?: string;
-  children: React.ReactNode;
-  as?: React.ElementType;
-  containerClassName?: string;
-  borderClassName?: string;
-  duration?: number;
-  className?: string;
-  [key: string]: any;
-}) {
-  return (
-    <Component
-      className={cn(
-        "bg-transparent relative text-xl p-[1px] overflow-hidden md:col-span-2 ",
-        containerClassName
-      )}
-      style={{
-        borderRadius: borderRadius,
-      }}
-      {...otherProps}
-    >
-      <div
-        className='absolute inset-0'
-        style={{ borderRadius: `calc(${borderRadius} * 0.96)` }}
-      >
-        <MovingBorder duration={duration} rx='30%' ry='30%'>
-          <div
-            className={cn(
-              "h-20 w-20 opacity-[0.8] bg-[radial-gradient(var(--sky-500)_40%,transparent_60%)]",
-              borderClassName
-            )}
-          />
-        </MovingBorder>
-      </div>
-
-      <div
-        className={cn(
-          "relative bg-slate-900/[0.8] border border-slate-800 backdrop-blur-xl text-white flex items-center justify-center w-full h-full text-sm antialiased",
-          className
-        )}
-        style={{
-          borderRadius: `calc(${borderRadius} * 0.96)`,
-        }}
-      >
-        {children}
-      </div>
-    </Component>
-  );
-}
 
 export const MovingBorder = ({
   children,
@@ -82,7 +20,7 @@ export const MovingBorder = ({
   ry?: string;
   [key: string]: any;
 }) => {
-  const pathRef = useRef<SVGPathElement>(null);
+  const pathRef = useRef<SVGRectElement>(null); // Change to SVGRectElement
   const progress = useMotionValue<number>(0);
 
   useAnimationFrame((time) => {
@@ -120,7 +58,7 @@ export const MovingBorder = ({
           height='100%'
           rx={rx}
           ry={ry}
-          ref={pathRef}
+          ref={pathRef} // Ref now matches SVGRectElement
         />
       </svg>
       <motion.div
