02b75f2d03c57865c6a183fed6b9fe0a0fee8963
Aaron Dalager <113053870+Ungabad@users.noreply.github.com>
2024-10-31 12:08:58 -0700

    Turned the Div in the PinContainer into an a tag

--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -28,14 +28,16 @@ export const PinContainer = ({
   };
 
   return (
-    <div
+    <a
+      href={href}
       className={cn(
         "relative group/pin z-50  cursor-pointer",
         containerClassName
       )}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
-      ref={href}
+      target='_blank' // optional if you want to open in a new tab
+      rel='noopener noreferrer' // optional for security with target="_blank"
     >
       <PinPerspective title={title} href={href} />
       <div
@@ -49,12 +51,12 @@ export const PinContainer = ({
           style={{
             transform: transform,
           }}
-          className='absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)] border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden'
+          className='absolute left-1/2 p-4 top-1/2 flex justify-start items-start rounded-2xl shadow-[0_8px_16px_rgb(0_0_0/0.4)] border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden'
         >
-          <div className={cn(" relative z-50 ", className)}>{children}</div>
+          <div className={cn("relative z-50", className)}>{children}</div>
         </div>
       </div>
-    </div>
+    </a>
   );
 };
 
